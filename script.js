b6430b5a1e1b18835596440c3f5c0b3d879ecef8
rcjdiv <62179956+rcjdiv@users.noreply.github.com>
2021-09-01 02:21:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-const toggle = document.getElementById('toggle');
-const body = document.querySelector('body');
-var div = document.getElementById('DSEGClock');
-    var seconds = document.getElementById('seconds-front');
-    var session = document.getElementById('sessin');
+const toggle = document.getElementById("toggle")
+const body = document.querySelector("body")
+var div = document.getElementById("DSEGClock")
+var seconds = document.getElementById("seconds-front")
+var session = document.getElementById("sessin")
 
-    function time() {
-        div.innerHTML = "";
-        var d = new Date();
-        var s = d.getSeconds();
-        var m = d.getMinutes();
-        var h = d.getHours();
-        var sesn = "AM";
+function time() {
+    div.innerHTML = ""
+    var d = new Date()
+    var s = d.getSeconds()
+    var m = d.getMinutes()
+    var h = d.getHours()
+    var sesn = "AM"
 
-        if(h == 0){
-            h = 12;
-        }
-        
-        if(h > 12){
-            h = h - 12;
-            sesn = "PM";
-        }
-        
-        h = (h < 10) ? "0" + h : h;
-        m = (m < 10) ? "0" + m : m;
-        s = (s < 10) ? "0" + s : s;
-
-        div.innerHTML = h + ":" + m;
-        seconds.innerHTML = s;
-        session.innerHTML = sesn;
-
+    if (h == 0) {
+        h = 12
     }
-    setInterval(time, 1000);
 
-    toggle.onclick = function (){
-        toggle.classList.toggle('active');
-        body.classList.toggle('active');
+    if (h > 12) {
+        h = h - 12
+        sesn = "PM"
     }
+
+    h = h < 10 ? "0" + h : h
+    m = m < 10 ? "0" + m : m
+    s = s < 10 ? "0" + s : s
+
+    div.innerHTML = h + ":" + m
+    seconds.innerHTML = s
+    session.innerHTML = sesn
+}
+
+toggle.onclick = function () {
+    toggle.classList.toggle("active")
+    body.classList.toggle("active")
+}
+
+setInterval(time, 1000)
+
+
+
